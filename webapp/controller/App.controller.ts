fdbadd384f0d08ab243ba36f21edce95ea9798b0
sbozbuga <sbozbuga@gmail.com>
2026-02-10 13:08:16 +0100

    Fixing EventBus Mismatch The "Open Settings" button is unresponsive because AdGuardService  publishes to the Core EventBus, while App.controller.ts  subscribes to the Component EventBus. I am updating App.controller.ts  to subscribe to the Core EventBus to match the Service.

--- webapp/controller/App.controller.ts
+++ webapp/controller/App.controller.ts
@@ -6,7 +6,13 @@ import UIComponent from "sap/ui/core/UIComponent";
  */
 export default class App extends BaseController {
     public onInit(): void {
-        const eventBus = this.getOwnerComponent()?.getEventBus();
-        eventBus?.subscribe("ui5.aghd", "openSettings", this.onOpenSettings, this);
+        const eventBus = sap.ui.getCore().getEventBus();
+        eventBus.subscribe("ui5.aghd", "openSettings", this.onOpenSettings, this);
+    }
+
+    public onExit(): void {
+        const eventBus = sap.ui.getCore().getEventBus();
+        eventBus.unsubscribe("ui5.aghd", "openSettings", this.onOpenSettings, this);
+        super.onExit();
     }
 }
